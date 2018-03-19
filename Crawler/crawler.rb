8b829f255d6afc2205077f46e8c0f84152d190c1
Mustii <mustii@mmk.one>
2018-03-19 16:11:57 +0300

    ignore is working

--- Crawler/crawler.rb
+++ Crawler/crawler.rb
@@ -1,17 +1,25 @@
 # Spidr is the library used to crawl the internet
 require 'spidr'
-
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
 =begin
     @param url is the url to be crawled
 =end
 
-def startCrawling(base_URL)
-
-    Spidr.site(base_URL) do |spider|
-        
-        spider.every_page do |page|
-            #TODO: parse the url & compress & save to database
-            puts ">>> #{page.url}"
-        end
+def startCrawling(base_URL,links_to_be_ignored = [])
+    
+    Spidr.site(base_URL, ignore_links: links_to_be_ignored) do |spider|
+        o = 0
+        spider.every_url do |url|
+            page = Nokogiri::HTML(open(url))
+            puts url
+            puts page.search('div.repository-content')
+            o += 1
+            if o == 100
+                return
+            end
+          end
     end
 end
+
--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 # Require the crawler from the directory
 require_relative 'Crawler/crawler'
-
+require_relative 'URL'
 # Starts the crawling proccess
-startCrawling('http://stackoverflow.com/')
+urls = Url.new()
+
+startCrawling(base_URL = urls.return_link_sof, links_to_be_ignored = urls.return_ignored_sof)
