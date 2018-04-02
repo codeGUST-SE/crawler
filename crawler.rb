7e52c082304dd0b62c7143caa2889ddb1a2dc7c1
evis-go <evis6174@gmail.com>
2018-04-02 16:16:07 +0300

    Created Crawler class

--- crawler.rb
+++ crawler.rb
@@ -7,37 +7,52 @@ require 'uri'
 =begin
     @param crawlable is the Crawlable object to be crawled
 =end
+class Crawler
+  MAX_CRAWLS = 20   # TODO: Determine the max number of crawls we want
 
-def start_crawling(crawlable)
-  Spidr.site(crawlable.url, ignore_links: crawlable.ignored_urls) do |spider|
-    o = 0
-    spider.every_url do |url|
-      link_object = CrawlableSites::Crawlable.new(url=url)
-      page = Nokogiri::HTML(open(url))
+  def initialize(crawlable)
+    @crawlable = crawlable
+  end
 
-      # searchs for the main components needed in crawlable object passed
-      crawlable.main_divs.each do |search_for|
-        parsed_page = page.search(search_for)
-        link_object.main_divs << parsed_page if parsed_page.count != 0
-      end
+  def start_crawling()
+    Spidr.site(@crawlable.url, ignore_links: @crawlable.ignored_urls) do |spider|
+      cnt = 0
+      spider.every_url do |url|
+        link_object = CrawlableSites::Crawlable.new(url=url)
+        page = Nokogiri::HTML(open(url))
 
-      # searchs for the scoring components needed in crawlable object passed
-      crawlable.score_divs.each do |search_for|
-        link_object.score_divs << page.search(search_for)
-      end if link_object.main_divs.count != 0
+        # searchs for the main components needed in crawlable object passed
+        @crawlable.main_divs.each do |search_for|
+          parsed_page = page.search(search_for)
+          link_object.main_divs << parsed_page if parsed_page.count != 0
+        end
 
-      ######################################################################
-      # TODO: should be removed! this is just for testing on personal computers
-      # test the output
-      puts ">>#{link_object.url}"
-      puts link_object.main_divs
-      puts ">>>>>"
-      puts link_object.score_divs
-      if o == 20
-        return
-      end
-      o += 1
-      ######################################################################
+        # searchs for the scoring components needed in crawlable object passed
+        @crawlable.score_divs.each do |search_for|
+          link_object.score_divs << page.search(search_for)
+        end if link_object.main_divs.count != 0
+
+        # skip this page if it does not contain the divs we need
+        if link_object.score_divs.empty?
+          next
+        end
+
+        ######################################################################
+        # TODO: should be removed! this is just for testing on personal computers
+        # test the output
+        puts ">>#{link_object.url}"
+        puts link_object.main_divs
+        puts ">>>>>"
+        puts link_object.score_divs
+        ######################################################################
+
+        # stop crawling after some number of pages
+        if cnt == MAX_CRAWLS
+          return
+        end
+        cnt += 1
       end
     end
+  end
+
 end
