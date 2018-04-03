5d1d78576a842625b52af79c2e04181e939e73c2
evis-go <evis6174@gmail.com>
2018-04-03 20:58:06 +0300

    Politeness policy implementation tweak that only waits for the remaining time.

--- crawler.rb
+++ crawler.rb
@@ -11,9 +11,12 @@ require 'uri'
 =end
 class Crawler
 
+  POLITENESS_POLICY_GAP = 30  # wait at least 30 seconds between each request
+
   def initialize(crawlable, limit)
     @crawlable = crawlable
     @max_crawls = limit
+    @last_request_time = 0
     @@dataset ||= Google::Cloud::Datastore.new(project_id: "codegust")
   end
 
@@ -21,8 +24,9 @@ class Crawler
     Spidr.site(@crawlable.url, ignore_links: @crawlable.ignored_urls) do |spider|
       cnt = 0
       spider.every_url do |url|
+
+        raw_page = get_page(url)
         crawled_page = CrawlablePages::Crawlable.new(url=url)
-        raw_page = Nokogiri::HTML(open(url))
 
         # searchs for the main components needed in crawlable object passed
         @crawlable.main_divs.each do |search_for|
@@ -43,12 +47,9 @@ class Crawler
         # save to Datastore
         add_to_datastore(crawled_page.url.to_s, crawled_page.main_divs.to_s)
 
-        # Crawler Politeness
-        sleep 5
-         
         puts crawled_page.url         # DEBUG
         puts crawled_page.score_divs  # DEBUG
-        
+
         # stop crawling after some number of pages
         if cnt == @max_crawls
           return
@@ -61,6 +62,18 @@ class Crawler
 
   private
 
+  # Requests and returns the page given the url.
+  # Waits if needed to comply to the politeness policy
+  def get_page(url)
+    time_passed = Time.now.to_i - @last_request_time
+    if time_passed < POLITENESS_POLICY_GAP
+      sleep(POLITENESS_POLICY_GAP - time_passed)
+    end
+    raw_page = Nokogiri::HTML(open(url))
+    @last_request_time = Time.now.to_i
+    raw_page
+  end
+
   def add_to_datastore(page_url, page_html)
     entity = Google::Cloud::Datastore::Entity.new
     entity.key = Google::Cloud::Datastore::Key.new "page", page_url
@@ -69,5 +82,5 @@ class Crawler
     entity.exclude_from_indexes! "page_html", true
     @@dataset.save entity
   end
-  
+
 end
